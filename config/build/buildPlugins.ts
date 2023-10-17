5f5361df155ef25c2f9eb91df50f2f0ebbd27dfd
Nikita <joyyolok@gmail.com>
2023-10-17 14:41:13 +0300

    fix yml

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,10 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
-import {type BuildConfig} from './types/config'
+import { type BuildConfig } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
-export function buildPlugins({paths, isDev}: BuildConfig): webpack.WebpackPluginInstance[] {
+export function buildPlugins ({ paths, isDev }: BuildConfig): webpack.WebpackPluginInstance[] {
     const plugins = [
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(true)
@@ -16,7 +16,7 @@ export function buildPlugins({paths, isDev}: BuildConfig): webpack.WebpackPlugin
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:5].css',
             chunkFilename: 'css/[name].[contenthash:5].css'
-        }),
+        })
     ]
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
