694d4884fdd9a8bad31d44ef2df207d727db9326
Nikita <joyyolok@gmail.com>
2023-10-17 14:31:55 +0300

    fix dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'app/provider/ThemeProvider'
 import 'shared/config/i18n/i18n'
