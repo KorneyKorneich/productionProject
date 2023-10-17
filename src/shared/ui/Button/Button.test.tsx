e5d02f327d36bf9e627217895b644231bc73c8a9
Nikita <joyyolok@gmail.com>
2023-10-17 16:50:59 +0300

    add translation

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,10 +3,12 @@ import Button, { ThemeButton } from './Button'
 
 describe('Button test', () => {
     test('Text inside', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button>Test</Button>)
         expect(screen.getByText('Test')).toBeInTheDocument()
     });
     test('With theme', () => {
+        // eslint-disable-next-line i18next/no-literal-string
         render(<Button theme={ThemeButton.CLEAR}>Test</Button>)
         expect(screen.getByText('Test')).toHaveClass('clear')
     })
