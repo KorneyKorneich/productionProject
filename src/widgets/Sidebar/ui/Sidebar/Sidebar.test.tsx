d368faf46c160819a4c5974f2a73ec70ed94b0b6
Nikita <joyyolok@gmail.com>
2023-10-17 23:56:40 +0300

    fix Sidebar test file

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,14 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react'
 import Sidebar from './Sidebar';
-import { renderWithTranslations } from '../../../../shared/lib/tests/renderWithTranslations/renderWithTranslations';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('Element exist', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('Sidebar')).toBeInTheDocument()
     });
     test('Test toggle', () => {
-        renderWithTranslations(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('Sidebar-toggle');
         expect(screen.getByTestId('Sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
