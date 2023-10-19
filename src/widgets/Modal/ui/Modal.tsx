82a1c5fc95b8b50677e6b5855255c4c4457f6c4e
Nikita <joyyolok@gmail.com>
2023-10-19 23:52:54 +0300

    fix global styles, fix gh actions

--- src/widgets/Modal/ui/Modal.tsx
+++ src/widgets/Modal/ui/Modal.tsx
@@ -56,12 +56,11 @@ const Modal = (props: ModalProps) => {
 
     const mods: Record<string, boolean> = {
         [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
-        [cls[theme]]: true
+        [cls.isClosing]: isClosing
     }
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content}
                         onClick={onContentClick}>
