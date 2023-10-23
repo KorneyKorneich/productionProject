ba7b466a14a27cc470f6327af8bbd80bf73986b8
Nikita <joyyolok@gmail.com>
2023-10-24 00:18:45 +0300

    fix test

--- src/app/provider/StoreProvider/config/store.ts
+++ src/app/provider/StoreProvider/config/store.ts
@@ -1,7 +1,7 @@
 import { configureStore, type ReducersMapObject } from '@reduxjs/toolkit'
 import { type StateSchema } from 'app/provider/StoreProvider/config/StateSchema';
 import { counterReducer } from '../../../../entities/Counter/model/slice/CounterSlice';
-import { userReducer } from 'entities/User';
+import { userReducer } from '../../../../entities/User/model/slice/userSlice';
 
 export function createReduxStore (initialState?: StateSchema) {
     const rootReducer: ReducersMapObject<StateSchema> = {
